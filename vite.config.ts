b632cfa2cdda30375a1db6c062307759afc63fc9
Pasha Kuznetsov <pasha@tydbits.com>
2026-05-27 20:09:45 -0700

    chore: build/dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,43 @@
+import { defineConfig } from 'vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [vue(), tailwindcss(), cloudflare()],
+export default defineConfig(({ command }) => ({
+  plugins: [
+    vue(),
+    tailwindcss(),
+    ...(command === 'serve'
+      ? [
+          cloudflare({
+            persistState: true,
+            auxiliaryWorkers: [
+              {
+                configPath: '../server/wrangler.jsonc',
+                config: (config) => {
+                  config.main = 'dev/entry.ts'
+                },
+              },
+            ],
+          }),
+        ]
+      : []),
+  ],
   resolve: {
     alias: {
       '@': new URL('./client', import.meta.url).pathname,
     },
   },
-})
+  server: {
+    // Forwards console.error, console.warn, and unhandled errors by default
+    forwardConsole: true
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, defaultHandler) {
+        if (warning.message?.includes('#__PURE__')) return
+        defaultHandler(warning)
+      },
+    },
+  },
+}))
